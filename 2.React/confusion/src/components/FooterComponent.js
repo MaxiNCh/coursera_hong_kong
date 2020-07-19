440644c81850000c6c0693db64513198b30ce245
MaxiNCh <mvzaboronkin@gmail.com>
2020-07-19 13:41:19 +0300

    React Router

--- 2.React/confusion/src/components/FooterComponent.js
+++ 2.React/confusion/src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 function Footer(props) {
 	return (
@@ -8,10 +9,10 @@ function Footer(props) {
 					<div className="col-4 offset-1 col-sm-2">
 						<h5>Links</h5>
 						<ul className="list-unstyled">
-							<li><a href="#">Home</a></li>
-							<li><a href="#">About</a></li>
-							<li><a href="#">Menu</a></li>
-							<li><a href="contactus.html">Contact</a></li>
+							<li><Link to="/home">Home</Link></li>
+							<li><Link to="/aboutus">About Us</Link></li>
+							<li><Link to="/menu">Menu</Link></li>
+							<li><Link to="/contactus">Contact US</Link></li>
 						</ul>
 					</div>
 					<div className="col-7 col-sm-5">
@@ -33,7 +34,7 @@ function Footer(props) {
 							<a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
 							<a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
 							<a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
-							<a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
+							<a className="btn btn-social-icon btn-secondary" href="mailto:"><i className="fa fa-envelope-o"></i></a>
 						</div>
 					</div>
 				</div>
--- 2.React/confusion/src/components/MainComponent.js
+++ 2.React/confusion/src/components/MainComponent.js
@@ -5,26 +5,37 @@ import Menu from './MenuComponent.js';
 import DishDetail from './DishDetailsComponent.js';
 import Header from './HeaderComponent.js';
 import Footer from './FooterComponent.js';
+import Home from './HomeComponent.js';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			dishes: DISHES,
-			selectedDish: null,
 		};
 	}
 
-	onDishSelect(dishId) {
-		this.setState({ selectedDish: dishId });
-	}
-
 	render() {
+
+		const HomePage = () => {
+			return (
+				<Home />
+			);
+		}
+
 		return (
 			<React.Fragment>
 				<Header />
-				<Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-				<DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+				<Switch>
+					<Route path='/home' > 
+						{HomePage}
+					</Route>
+					<Route exact path='/menu'>
+						<Menu dishes={this.state.dishes} />
+					</Route>
+					<Redirect to='/home' />
+				</Switch>
 				<Footer />
 			</React.Fragment>
 		);		
