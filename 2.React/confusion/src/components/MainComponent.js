b7f57e33ba8d984762510ded2db48ae91110fcc8
MaxiNCh <mvzaboronkin@gmail.com>
2020-07-25 21:37:13 +0300

    React Redux Form

--- 2.React/confusion/src/components/MainComponent.js
+++ 2.React/confusion/src/components/MainComponent.js
@@ -1,8 +1,4 @@
 import React, {Component} from 'react';
-import {DISHES} from '../shared/dishes.js';
-import { COMMENTS } from '../shared/comments.js';
-import { PROMOTIONS } from '../shared/promotions.js';
-import { LEADERS } from '../shared/leaders.js';
 import Menu from './MenuComponent.js';
 import DishDetail from './DishDetailsComponent.js';
 import Header from './HeaderComponent.js';
@@ -23,10 +19,9 @@ const mapStateToProps = state => {
 }
 
 class Main extends Component {
-	constructor(props) {
-		super(props);
-		};
-	}
+	// constructor(props) {
+	// 	super(props);
+	// }
 
 	render() {
 
